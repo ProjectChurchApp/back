c99ee98482f4076f83b0eaea9d08929a1e02cd0d
Korcp <pm6567@naver.com>
2026-01-23 22:06:23 +0900

    Intial Commit

---
+++ src/main/java/com/church/app/Security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.church.app.Security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .cors(Customizer.withDefaults())
+
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
