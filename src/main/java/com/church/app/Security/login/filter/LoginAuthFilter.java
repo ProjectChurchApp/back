4580d62d5a4e96a46646fec3e09b577ec70ebd00
Korcp <pm6567@naver.com>
2026-02-24 22:30:02 +0900

    feat : Token 발급 및 리프레쉬 토큰 쿠키 반환

--- src/main/java/com/church/app/Security/login/filter/LoginAuthFilter.java
+++ src/main/java/com/church/app/Security/login/filter/LoginAuthFilter.java
@@ -2,9 +2,12 @@ package com.church.app.Security.login.filter;
 
 import com.church.app.Security.login.dto.LoginRequest;
 import com.church.app.Security.login.dto.LoginResponse;
+import com.church.app.Security.login.service.RefreshTokenService;
+import com.church.app.Security.login.utils.JwtUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,8 +23,12 @@ import java.io.IOException;
 
 public class LoginAuthFilter extends AbstractAuthenticationProcessingFilter {
     private final ObjectMapper objectMapper = new ObjectMapper();
-    public LoginAuthFilter(AuthenticationManager authenticationManager) {
+    private final JwtUtils jwtUtils;
+    private final RefreshTokenService refreshTokenService;
+    public LoginAuthFilter(AuthenticationManager authenticationManager, JwtUtils jwtUtils1, RefreshTokenService refreshTokenService1) {
         super(new AntPathRequestMatcher("/auth/login", "POST"));
+        this.jwtUtils = jwtUtils1;
+        this.refreshTokenService = refreshTokenService1;
         this.setAuthenticationManager(authenticationManager);
     }
 
@@ -47,13 +54,26 @@ public class LoginAuthFilter extends AbstractAuthenticationProcessingFilter {
                 .map(GrantedAuthority::getAuthority)
                 .orElse("ROLE_ANONYMOUS");
 
+            String accessToken = jwtUtils.generateAccessToken(authResult);
+
+            String refreshToken = refreshTokenService.createRefreshToken(loginID);
+
             SecurityContextHolder.getContext().setAuthentication(authResult);
 
             response.setStatus(HttpServletResponse.SC_OK);
             response.setContentType("application/json; charset=utf-8");
 
+            response.addHeader("Authorization", "Bearer " + accessToken);
+
+             Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+             refreshTokenCookie.setHttpOnly(true);  // XSS 방어
+             refreshTokenCookie.setSecure(true);    // HTTPS만
+             refreshTokenCookie.setPath("/");
+             refreshTokenCookie.setMaxAge(14 * 24 * 60 * 60);  // 14일
+             response.addCookie(refreshTokenCookie);
+
             LoginResponse loginResponse = new LoginResponse(
-                    true,"로그인 성공", loginID,role,null
+                    true,"로그인 성공", loginID,role,accessToken
             );
 
             response.getWriter().write(objectMapper.writeValueAsString(loginResponse));
