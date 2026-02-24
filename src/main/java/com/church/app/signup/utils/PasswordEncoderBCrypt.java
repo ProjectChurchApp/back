e1b13e0f0f85c01bada6ade69b0d1551703b2ab7
Korcp <pm6567@naver.com>
2026-02-24 21:12:04 +0900

    refactor : PasswordEncoder 빈등록 후 사용방식으로 변경

--- src/main/java/com/church/app/signup/utils/PasswordEncoderBCrypt.java
+++ src/main/java/com/church/app/signup/utils/PasswordEncoderBCrypt.java
@@ -1,34 +1,15 @@
 package com.church.app.signup.utils;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Component
+
+@Configuration
 public class PasswordEncoderBCrypt {
-    private final BCryptPasswordEncoder encoder;
-    public PasswordEncoderBCrypt(BCryptPasswordEncoder encoder) {
-        this.encoder = encoder;
-    }
-
-    /**
-     * 비밀번호 암호화
-     */
-    public String encode(CharSequence rawPassword) throws PasswordEncodeException {
-        try {
-            return encoder.encode(rawPassword);
-        } catch (Exception e) {
-            throw new PasswordEncodeException("비밀번호 암호화 중 오류가 발생했습니다.", e);
-        }
-    }
-
-    /**
-     * 비밀번호 일치 여부 확인
-     */
-    public boolean matches(CharSequence rawPassword, String encodedPassword) throws PasswordEncodeException {
-        try {
-            return encoder.matches(rawPassword, encodedPassword);
-        } catch (Exception e) {
-            throw new PasswordEncodeException("비밀번호 검증 중 오류가 발생했습니다.", e);
-        }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
