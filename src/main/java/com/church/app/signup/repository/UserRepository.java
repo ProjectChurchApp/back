01f44e90cd3b987992e16f11c65a78a7579b5b6d
Korcp <pm6567@naver.com>
2026-02-24 21:11:22 +0900

    rename : SignupRepository -> UserRepostiory로 변경

--- src/main/java/com/church/app/signup/repository/UserRepository.java
+++ src/main/java/com/church/app/signup/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.church.app.signup.repository;
 import com.church.app.signup.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByLoginID(String loginID);
 
     boolean existsByloginID(String loginID);
 }
